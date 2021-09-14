41b8e70c71868e1bb135ddf0680fe3e89a605b71
knaevKMK <kneav@students.softuni.bg>
2021-09-14 23:16:28 +0300

    add

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -4,15 +4,27 @@ import Book from "./Book";
 class BookList extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { count: 1 };
+        this.state = { count: 3 };
+        this.resetCounter = this.resetCounter.bind(this);
     }
 
+    decrementCounter() {
+        this.setState(oldState => ({ count: oldState.count - 1 }));
+    }
+
+    incrementCounter() {
+        this.setState(oldState => ({ count: oldState.count + 1 }));
+    }
+    resetCounter() {
+        this.setState({ count: 0 });
+    }
     render() {
         return (
             <ul className="App-header">
-                <button onClick={(e) => this.setState({ count: this.state.count - 1 })} >-</button>
+                <button onClick={(e) => this.decrementCounter()} >-</button>
                 <span>{this.state.count}</span>
-                <button onClick={(e) => this.setState({ count: this.state.count + 1 })} >+</button>
+                <button onClick={this.incrementCounter.bind(this)} >+</button>
+                <button onClick={this.resetCounter} >Reset</button>
                 <h2>Our Collection</h2>
                 {this.props.books.map(b => <Book title={b.title} description={b.description} />)}
 
